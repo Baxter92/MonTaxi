6964dbde75e7ec6975bbed74e71ab94725b38c4c
ulrichdev <ulrsinha@gmail.com>
2020-07-04 21:40:31 +0100

    _

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText countryEdt;
+    private EditText numberEdt;
+    private EditText passwordEdt;
+    private Button gotIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recovery2);
+        init();
+    }
+
+    private void init() {
+        countryEdt = findViewById(R.id.country);
+        numberEdt = findViewById(R.id.phone);
+        passwordEdt = findViewById(R.id.password);
+        gotIt = findViewById(R.id.email_sign_in_button);gotIt.setOnClickListener(this);
+        numberEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (i2 >= 6){
+                    numberEdt.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.circle_green, 0);
+                }else {
+                    numberEdt.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.circle_grey, 0);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
