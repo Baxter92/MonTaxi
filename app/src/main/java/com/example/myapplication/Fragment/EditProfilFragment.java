6f58c36a550e43b063b18b2fa4e62883c5fac9e4
Ulrich Sinha <ulrsinha@gmail.com>
2020-08-10 09:23:09 +0100

    _

--- app/src/main/java/com/example/myapplication/Fragment/EditProfilFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/EditProfilFragment.java
@@ -17,6 +17,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.myapplication.Models.Profil;
 import com.example.myapplication.Models.Town;
@@ -36,14 +37,16 @@ import java.util.List;
  * Use the {@link EditProfilFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EditProfilFragment extends Fragment {
+public class EditProfilFragment extends Fragment implements View.OnClickListener {
 
     private EditText passEdt;
     private Button imageUpload;
     private LinearLayout ll_btn;
     SessionDriver sessionDriver;
     private EditText first_nameEdt, last_nameEdt, phone_Edt;
-    private Button cancelBtn, updateBtn;
+    private Button cancelBtn, updateBtn, resetBtn;
+    private ImageView update_firstname, update_lastname;
+    TextView updatedone;
     private Spinner spinner;
     String[] towns;
     ArrayAdapter<String> adapter;
@@ -75,57 +78,17 @@ public class EditProfilFragment extends Fragment {
         adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item, towns);
         spinner.setAdapter(adapter);
 
-        imageUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SwitchFragment(UploadProfilFragment.newInstance());
-            }
-        });
-        passEdt.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(passEdt.getCompoundDrawables()[2]!=null){
-                        if(event.getX() >= (passEdt.getRight()- passEdt.getLeft() - passEdt.getCompoundDrawables()[2].getBounds().width())) {
-                           SwitchFragment(PasswordProfilFragment.newInstance());
-                        }
-                    }
-                }
-                return false;
-            }
-        });
+        imageUpload.setOnClickListener(this);
+
         return view;
     }
     private void initEnableView(){
-       // first_nameEdt.setEnabled(false);
-        //last_nameEdt.setEnabled(false);
-        //passEdt.setEnabled(false);
-        //ll_btn.setVisibility(View.GONE);
-
-        EditProfil();
+        first_nameEdt.setEnabled(false);
+        last_nameEdt.setEnabled(false);
+        passEdt.setEnabled(false);
+        ll_btn.setVisibility(View.GONE);
     }
 
-    private void EditProfil(){
-        first_nameEdt.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if (!first_nameEdt.getText().toString().isEmpty()){
-                    ll_btn.setVisibility(View.VISIBLE);
-                }
-                return false;
-            }
-        });
-
-        last_nameEdt.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if (!last_nameEdt.getText().toString().isEmpty()){
-                    ll_btn.setVisibility(View.VISIBLE);
-                }
-                return false;
-            }
-        });
-    }
     private void init(View view) {
         passEdt = (EditText)view.findViewById(R.id.password);
         imageUpload = (Button) view.findViewById(R.id.upload);
@@ -136,23 +99,18 @@ public class EditProfilFragment extends Fragment {
         cancelBtn = (Button)view.findViewById(R.id.cancel);
         updateBtn = (Button)view.findViewById(R.id.update);
         spinner = (Spinner)view.findViewById(R.id.spinner2);
+        update_firstname = (ImageView)view.findViewById(R.id.update_firstname);
+        update_lastname = (ImageView)view.findViewById(R.id.update_lastname);
+        resetBtn = (Button)view.findViewById(R.id.resetpwd);
+        updatedone = (TextView)view.findViewById(R.id.done);
 
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initEnableView();
-            }
-        });
+        update_lastname.setOnClickListener(this);
+        update_firstname.setOnClickListener(this);
+        resetBtn.setOnClickListener(this);
 
-        updateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sessionDriver.getDriver().getProfil().setFirst_name(first_nameEdt.getText().toString());
-                sessionDriver.getDriver().getProfil().setFirst_name(last_nameEdt.getText().toString());
-                setValue();
-                initEnableView();
-            }
-        });
+        cancelBtn.setOnClickListener(this);
+
+        updateBtn.setOnClickListener(this);
         setValue();
         initEnableView();
         //setValueTest();
@@ -165,6 +123,9 @@ public class EditProfilFragment extends Fragment {
         phone_Edt.setText(sessionDriver.getDriver().getNumber());
         phone_Edt.setEnabled(false);
         passEdt.setText(profil.getPassword());
+        updatedone.setText(getString(R.string.update_done));
+        updatedone.setTextColor(getResources().getColor(R.color.commentsent));
+        initEnableView();
     }
     private void setValueTest() {
         first_nameEdt.setText("Ulrich");
@@ -201,5 +162,33 @@ public class EditProfilFragment extends Fragment {
             Log.d("townvalue","bus after "+eventTown.getTowns().get(i).getName());
         }
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.update_firstname:
+                first_nameEdt.setEnabled(true);
+                break;
+            case R.id.update_lastname:
+                last_nameEdt.setEnabled(true);
+                break;
+            case R.id.resetpwd:
+                SwitchFragment(PasswordProfilFragment.newInstance());
+                break;
+            case R.id.update:
+                sessionDriver.getDriver().getProfil().setFirst_name(first_nameEdt.getText().toString());
+                sessionDriver.getDriver().getProfil().setFirst_name(last_nameEdt.getText().toString());
+                setValue();
+                break;
+            case R.id.cancel:
+                initEnableView();
+                break;
+            case R.id.upload:
+                SwitchFragment(UploadProfilFragment.newInstance());
+                break;
+            default:
+                break;
+        }
     }
 }
